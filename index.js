27c5e81a6f8f4bd215582232377fc27751a189dd
Dera <deraegbo@gmail.com>
2023-10-23 23:38:25 +0100

    added some more parameters to cors()

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   },
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions, {credentials: true, origin: true}));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 
