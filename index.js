10f3240854347e54a7c6aa8b079e085f7557570a
Dera <deraegbo@gmail.com>
2023-10-23 10:18:25 +0100

    Added a get route for the entry point '/'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser, { json } from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -11,11 +11,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({}));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 
 app.use('/academy/roster', rosterRoutes);
+
+app.get('/', (req, res) => {
+  res.send('APP IS RUNNING');
+});
 
 mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
   .then( () => app.listen(PORT, () => console.log(`App running @ ${PORT}`)) )
