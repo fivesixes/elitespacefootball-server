3fb6d62c392a69ab431cb6341169a55086b00e5f
Dera <deraegbo@gmail.com>
2023-10-24 09:08:25 +0100

    Further cors() changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.options('*', cors());
 var allowCrossDomain = function(req,res,next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();  
 }
 app.use(allowCrossDomain);
